f48417149fe124d5dba5ba4148c430824a6032d4
Marlou Dev <marlou.dev.app@gmail.com>
2025-02-22 22:01:56 +0800

    dakjlwdwla

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -11,13 +11,12 @@ use Illuminate\Support\Facades\Auth;
 class AnswerController extends Controller
 {
 
-    public function get_score(Request $request,$id, $type)
+    public function get_score(Request $request, $id, $type)
     {
-        // 1/pre-exercise
         $score = Answer::where([
             ['learning_id', '=', $id],
             ['type', '=', $type],
-            ['user_id','=',$request->user_id]
+            ['student_id', '=', $request->user_id]
         ]);
 
         $over = Quest::where([
@@ -25,17 +24,18 @@ class AnswerController extends Controller
             ['type', '=', $type],
         ]);
 
-        $total = ($score / $over->count())  * 100;
+
+        $total = ($score->sum('score') / $over->count())  * 100;
         $status = $total >= 75 ? "Passed" : "Failed";
 
         if ($type == 'assessment' && $status == 'Failed') {
-            $over->delete();
+            $score->delete();
         }
 
         return response()->json([
             'status' => 'success',
             'answer_status' => $status,
-            'data' => $score,
+            'data' => $score->sum('score'),
             'over' => $over->count(),
         ], 200);
     }
@@ -63,10 +63,8 @@ class AnswerController extends Controller
 
 
         $quest = Quest::where('id', $request->quest_id)->first();
-        $answer = Answer::where([
-            ['quest_id', '=', $request->quest_id],
-            ['student_id', '=', $request->student_id],
-        ])->first();
+        $answer = Answer::where('quest_id', '=', $request->quest_id)->first();
+
 
 
         if (!$answer) {
@@ -82,10 +80,8 @@ class AnswerController extends Controller
         $isFinish = false;
         $count_answer = Answer::where([
             ['learning_id', '=', $quest->learning_id],
-            ['type', '=', $quest->type],
-            ['student_id', '=', $quest->student_id],
+            ['type', '=', $quest->type]
         ])->count();
-
         $count_quest = Quest::where([
             ['learning_id', '=', $quest->learning_id],
             ['type', '=', $quest->type]
