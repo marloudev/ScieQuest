38e8e89cf55d623cfa0143a3b041419c2c053b33
QuicklyDG <quicklydeguzman@gmail.com>
2025-02-28 18:57:50 +0800

    created restriction for create lesson

--- resources/js/app/pages/admin/module/id/page.jsx
+++ resources/js/app/pages/admin/module/id/page.jsx
@@ -8,17 +8,23 @@ import store from "@/app/pages/store/store";
 import AssessmentCardSection from "./sections/assessment-card-section";
 import CreateLessonSection from "./sections/create-lesson-section";
 import { get_module_by_id_thunk } from "../redux/booklet-thunk";
+import { useSelector } from "react-redux";
 
 export default function LiteracyTestIDPage() {
     const module_id = window.location.pathname.split("/")[3];
+    const { user } = useSelector((state) => state.app);
     useEffect(() => {
         store.dispatch(get_module_by_id_thunk(module_id));
     }, []);
+    console.log('user', user)
     return (
         <AdminLayout>
-            <div className="my-3">
-                <CreateLessonSection />
-            </div>
+            {user?.user_type == 1 && (
+                <div className="my-3">
+                    <CreateLessonSection />
+                </div>
+            )}
+
             <div>
                 <AssessmentCardSection />
             </div>
